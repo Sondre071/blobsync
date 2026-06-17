48db061ac7b441da85fca91c980e0c2841cea853
Sondre071 <sirciller@gmail.com>
2026-06-17 18:22:36 +0200

    UI: Organize container list into side panel container

--- src/app.rs
+++ src/app.rs
@@ -8,17 +8,24 @@ pub struct App {
 #[derive(Default)]
 struct State {
     containers: Vec<String>,
+
+    current_container: String,
+    current_blobs: Vec<String>,
 }
 
 pub enum Message {
     Containers(Vec<String>),
+    Blobs {
+        container: String,
+        blobs: Vec<String>,
+    },
 }
 
 impl Default for App {
     fn default() -> Self {
         let backend = Backend::new();
         backend.list_containers();
-        
+
         Self {
             state: State::default(),
             backend,
@@ -27,30 +34,38 @@ impl Default for App {
 }
 
 impl eframe::App for App {
-    fn ui(&mut self, ctx: &mut egui::Ui, _: &mut eframe::Frame) {
-        // Runtime loop
-        egui::CentralPanel::default().show_inside(ctx, |ui| {
+    // Runtime loop
+    fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame) {
+        egui::Panel::left("left_side_list")
+            .min_size(150.0)
+            .show_inside(ui, |ui| {
+                ui.add_space(5.0);
+                ui.add_sized(
+                    [ui.available_width(), 25.0],
+                    egui::Label::new(egui::RichText::new("Containers").heading()),
+                );
+
+                ui.separator();
+
+                egui::ScrollArea::vertical()
+                    .auto_shrink(false)
+                    .show(ui, |ui| {
+                        for container in self.state.containers.iter() {
+                            if ui.button(container).clicked() {
+                                // fetch list of blobs
+                            }
+                        }
+                    });
+            });
+
+        egui::CentralPanel::default().show_inside(ui, |ui| {
             // Check incoming data
             while let Ok(msg) = self.backend.receiver.try_recv() {
                 match msg {
                     Message::Containers(names) => self.state.containers = names,
+                    Message::Blobs { container, blobs } => {}
                 }
             }
-
-            // Render UI
-
-            if self.state.containers.is_empty() {
-                if ui.button("Fetch containers").clicked() {
-                    self.backend.list_containers();
-                }
-            } else {
-                for container in self.state.containers.iter() {
-                    if ui.button(container).clicked() {
-                        //self.backend.
-                    }
-                }
-            }
-            ui.label("No containers");
         });
     }
 }
