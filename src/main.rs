854ab64dcd191a7f54749fe605cbcf4309d6034b
Sondre071 <sirciller@gmail.com>
2026-06-17 16:35:26 +0200

    Milestone: Able to fetch and render all container names

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,88 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 mod shared;
 
-use eframe;
-use egui;
+use std::sync::mpsc::{Receiver, Sender, channel};
 
 fn main() -> eframe::Result {
-    eframe::run_native("blobsync",
-        Default::default(),
-        Box::new(|_|
-            Ok(Box::<App>::default())
-        )
-    )
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([1000.0, 800.0])
+            .with_min_inner_size([1000.0, 800.0])
+            .with_transparent(true),
+        ..Default::default()
+    };
+
+    eframe::run_native("blobsync", options, Box::new(|_| Ok(Box::<App>::default())))
+}
+
+struct App {
+    state: State,
+    backend: Backend,
 }
 
 #[derive(Default)]
-struct App {
-    screen: usize,
+struct State {
+    containers: Vec<String>,
+}
+
+struct Backend {
+    runtime: tokio::runtime::Runtime,
+    sender: Sender<Message>,
+    receiver: Receiver<Message>,
+}
+
+enum Message {
+    Containers(Vec<String>),
+}
+
+impl Default for App {
+    fn default() -> Self {
+        let (sender, receiver) = channel();
+
+        Self {
+            state: State::default(),
+            backend: Backend {
+                runtime: tokio::runtime::Runtime::new().unwrap(),
+                sender,
+                receiver,
+            },
+        }
+    }
 }
 
 impl eframe::App for App {
-    fn update(&mut self, ctx: &egui::Context, _: &mut eframe::Frame) {}
+    fn ui(&mut self, ctx: &mut egui::Ui, _: &mut eframe::Frame) {
+        
+        // Runtime loop
+        egui::CentralPanel::default().show_inside(ctx, |ui| {
+            
+            // Check incoming data
+            while let Ok(msg) = self.backend.receiver.try_recv() {
+                match msg {
+                    Message::Containers(names) => self.state.containers = names,
+                }
+            }
 
-    fn ui(&mut self, ctx: &mut egui::Ui, _: &mut eframe::Frame) {}
+            // Render UI
+            
+            if self.state.containers.is_empty() {
+                if ui.button("Fetch containers").clicked() {
+                    let sender = self.backend.sender.clone();
+
+                    self.backend.runtime.spawn(async move {
+                        let names = shared::list_containers().await.unwrap_or_default();
+                        sender
+                            .send(Message::Containers(names))
+                            .expect("Failed to container data from thread.");
+                    });
+                }
+            } else {
+                for container in self.state.containers.iter() {
+                    ui.label(container);
+                }
+            }
+            ui.label("No containers");
+        });
+    }
 }
--- src/shared/list_containers.rs
+++ src/shared/list_containers.rs
@@ -4,22 +4,22 @@ use azure_core::http::Url;
 use azure_storage_blob::*;
 use futures::TryStreamExt;
 
-pub async fn list_containers() -> std::result::Result<(), Box<dyn std::error::Error>> {
-    println!("Hello, world!");
-
+pub async fn list_containers() -> std::result::Result<Vec<String>, Box<dyn std::error::Error>> {
     let account = shared::statics::get_container_account()?;
     let service_url = Url::parse(&format!("{}?{}", account.endpoint, account.sas))?;
 
     let service_client = BlobServiceClient::new(service_url, None, None)?;
     let mut pager = service_client.list_containers(None)?.into_pages();
 
+    let mut containers = Vec::<String>::new();
+
     while let Some(page) = pager.try_next().await? {
         let list = page.into_model()?;
 
         for item in list.container_items {
-            println!("{}", item.name.unwrap());
+            containers.push(item.name.unwrap());
         }
     }
 
-    Ok(())
+    Ok(containers)
 }
