64b0fe05f7d0369f1ee3214ea1d1af8ee1dff571
Sondre071 <sirciller@gmail.com>
2026-06-18 23:43:42 +0200

    Move yet-to-be-developed file to a more fitting module

--- src/shared/index.rs
+++ src/shared/index.rs
@@ -1,9 +1,9 @@
-use super::Backend;
+use super::Shared;
 
 /*
  * Index local files and cache the results.
  */
-impl Backend {
+impl Shared {
     fn index_local_files(&self) {
         todo!()
     }
